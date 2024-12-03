f0f65aa5eba221a3715e41a3a80b7714463ac381
Raghav Pillai <raghavpillai101@gmail.com>
2024-12-02 18:14:51 -0800

    Refactor XML parsing with safer string handling
    - Introduced  class to handle XML parsing safely
    - Removed  class and related code
    - Updated dependencies to use  and
    - Cleaned up debug print statements in parser

--- llmxml/__init__.py
+++ llmxml/__init__.py
@@ -1,4 +1,4 @@
-from .parser import CodeContent, parse_xml
+from .parser import XMLSafeString, parse_xml
 from .prompting import generate_prompt_template
 
-__all__ = ["parse_xml", "generate_prompt_template", "CodeContent"]
+__all__ = ["parse_xml", "generate_prompt_template", "XMLSafeString"]
